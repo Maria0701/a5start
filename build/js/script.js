a81c3f1e80dc9a7f764fa4f27cdcf12368fe8eb8
Maria <reflection2008@gmail.com>
2020-02-23 17:23:24 +0300

    accordeon

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,43 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
-
-pageHeader.classList.remove('page-header--nojs');
-
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+(function () {
+  var elementToClip = document.querySelector('.js-clip');
+  var textSize = 439;
+  var tabletScreen = 768;
+  var elipsis = '...';
+  var text = elementToClip.innerHTML;
+  if (text.length > textSize && screen.width <= tabletScreen) {
+    text = text.substr(0, textSize);
+    elementToClip.innerHTML = text + elipsis;
   }
-});
+}());
+
+(function () {
+  var footerButtons = document.querySelectorAll('.footer__button ');
+  var closeAllButtons = function () {
+    footerButtons.forEach(function (button) {
+      if (!button.classList.contains('js-closed')) {
+        button.classList.add('js-closed');
+        button.nextElementSibling.style.maxHeight = null;
+      }
+    });
+  };
+
+  footerButtons.forEach(function (item) {
+    item.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      var infoElement = item.nextElementSibling;
+console.log(infoElement.scrollHeight);
+      if (!item.classList.contains('js-closed')) {
+        item.classList.add('js-closed');
+        infoElement.style.maxHeight = null;
+        return;
+      }
+
+      closeAllButtons();
+      item.classList.remove('js-closed');
+      console.log(infoElement.scrollHeight);
+      infoElement.style.maxHeight = infoElement.scrollHeight + 'px';
+    });
+  });
+
+}());
--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,12 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
-
-pageHeader.classList.remove('page-header--nojs');
-
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+(function () {
+  var elementToClip = document.querySelector('.js-clip');
+  var textSize = 439;
+  var tabletScreen = 768;
+  var elipsis = '...';
+  var text = elementToClip.innerHTML;
+  if (text.length > textSize && screen.width <= tabletScreen) {
+    text = text.substr(0, textSize);
+    elementToClip.innerHTML = text + elipsis;
   }
-});
+}());
