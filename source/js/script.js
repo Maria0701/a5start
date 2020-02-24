4c62579b998160fbf16dd88300ba40ed9ce9fbfc
Maria <reflection2008@gmail.com>
2020-02-24 22:44:02 +0300

    исправлен js

--- source/js/script.js
+++ source/js/script.js
@@ -40,10 +40,157 @@
 }());
 
 (function () {
+  window.maskPhoneNumper = function (formBlock, hide) {
+    var MAIN_KEYCODES = {
+      BACKSPACE: 8,
+      LEFT_ARROW: 37,
+      RIGHT_ARROW: 39,
+      DELETE: 46,
+    };
+
+    var phoneField = formBlock.querySelector('.js-phone');
+    var textField = formBlock.querySelector('.form__textarea');
+    var enterFirstLetters = function (evt) {
+      evt.preventDefault();
+      phoneField.value = '+7 (';
+
+      phoneField.removeEventListener('keydown', enterFirstLetters);
+      phoneField.removeEventListener('click', enterFirstLetters);
+
+      var oldValueLength = phoneField.value.length;
+      phoneField.addEventListener('keydown', function (pressedKey) {
+        if (!((pressedKey.key >= 0 && pressedKey.key <= 9) || pressedKey.keyCode === MAIN_KEYCODES.BACKSPACE || pressedKey.keyCode === MAIN_KEYCODES.DELETE || pressedKey.keyCode === MAIN_KEYCODES.LEFT_ARROW || pressedKey.keyCode === MAIN_KEYCODES.RIGHT_ARROW)) {
+          pressedKey.preventDefault();
+        }
+
+        var currentLength = phoneField.value.length;
+        if (currentLength < oldValueLength) {
+          oldValueLength--;
+          return;
+        }
+
+        if (currentLength < 5) {
+          phoneField.value = '+7 (';
+        }
+
+        if (currentLength === 7) {
+          phoneField.value = phoneField.value + ') ';
+        }
+
+        if (currentLength === 12) {
+          phoneField.value = phoneField.value + '-';
+        }
+
+        if (currentLength === 15) {
+          phoneField.value = phoneField.value + '-';
+        }
+
+        if ((currentLength > 17) && pressedKey.keyCode !== MAIN_KEYCODES.BACKSPACE && pressedKey.keyCode !== MAIN_KEYCODES.DELETE && pressedKey.keyCode !== MAIN_KEYCODES.LEFT_ARROW && pressedKey.keyCode !== MAIN_KEYCODES.RIGHT_ARROW) {
+          textField.focus();
+        }
+
+        oldValueLength++;
+      });
+    };
+
+    phoneField.addEventListener('keydown', enterFirstLetters);
+    phoneField.addEventListener('click', enterFirstLetters);
+
+    if (hide) {
+      phoneField.removeEventListener('keydown', enterFirstLetters);
+      phoneField.removeEventListener('click', enterFirstLetters);
+    }
+  };
+})();
+
+(function () {
+  var setCustomValidation = function (input) {
+    var fieldValidate = input.validity;
+    if (input.type !== 'submit' && input.type !== 'checkbox') {
+      if (fieldValidate.valueMissing) {
+        input.setCustomValidity('Заполните поле');
+      } else if (fieldValidate.tooShort) {
+        input.setCustomValidity('Мало букв');
+      } else if (fieldValidate.tooLong) {
+        input.setCustomValidity('Много букв');
+      } else if (fieldValidate.patternMismatch) {
+        input.setCustomValidity('не подходит по паттерну');
+      } else {
+
+        input.setCustomValidity('');
+      }
+    } else if (input.type === 'checkbox') {
+      if (fieldValidate.valueMissing) {
+        input.setCustomValidity('Согласитесь с правилами обработки данных');
+      } else {
+        input.setCustomValidity('');
+      }
+    }
+  };
+
+  window.formValidate = function (form, hide) {
+    Array.from(form.elements).forEach(function (it) {
+      if (hide) {
+        it.removeEventListener('invalid', setCustomValidation(it));
+        return;
+      }
+      it.addEventListener('invalid', setCustomValidation(it));
+    });
+  };
+})();
+
+(function () {
+  window.setLocalStorage = function (form) {
+    var nameField = form.querySelector('[name=name]');
+    var phoneField = form.querySelector('[name=phone]');
+    var messageField = form.querySelector('[name=message]');
+    var isStorageSupport = true;
+    var storage = {
+      name: '',
+      phone: '',
+      message: '',
+    };
+
+    try {
+      storage.name = localStorage.getItem('nameField');
+      storage.phone = localStorage.getItem('phoneField');
+      storage.message = localStorage.getItem('messageField');
+    } catch (err) {
+      isStorageSupport = false;
+    }
+
+    form.addEventListener('submit', function () {
+      window.formValidate(form, false);
+      if (isStorageSupport) {
+        localStorage.setItem('nameField', nameField.value);
+        localStorage.setItem('phoneField', phoneField.value);
+        localStorage.setItem('messageField', messageField.value);
+      }
+    });
+
+    if (!storage.name) {
+      nameField.focus();
+    }
+    if (storage.name !== '') {
+      nameField.value = storage.name;
+      phoneField.focus();
+    }
+    if (storage.phone !== '') {
+      phoneField.value = storage.phone;
+      messageField.focus();
+    }
+    if (storage.message !== '') {
+      messageField.value = storage.message;
+    }
+  };
+})();
+
+(function () {
   var popupOpeners = document.querySelectorAll('.js-popup-open');
   var popupBlock = document.querySelector('.popup');
   var popupCloser = popupBlock.querySelector('.popup__close');
   var overlayBlock = document.querySelector('.overlay');
+  var popupForm = popupBlock.querySelector('.popup__form');
   var ESCAPE_BUTTON = 27;
 
   var popupCloseHandler = function (evt) {
@@ -51,6 +198,8 @@
     popupBlock.classList.remove('js-popup-show');
     overlayBlock.removeEventListener('click', popupCloseHandler);
     document.removeEventListener('keydown', popupEscPressHandler);
+    window.maskPhoneNumper(popupForm, true);
+    window.formValidate(popupForm, true);
   };
 
   var popupEscPressHandler = function (evt) {
@@ -65,9 +214,17 @@
     popupCloser.addEventListener('click', popupCloseHandler);
     overlayBlock.addEventListener('click', popupCloseHandler);
     document.addEventListener('keydown', popupEscPressHandler);
+    window.maskPhoneNumper(popupForm, false);
+    window.setLocalStorage(popupForm);
   };
 
   popupOpeners.forEach(function (item) {
     item.addEventListener('click', popupHandler);
   });
-}());
+})();
+
+(function () {
+  var form = document.querySelector('.feedback__form');
+  window.maskPhoneNumper(form, false);
+  window.setLocalStorage(form);
+})();
