14e624574a5db9087c8b504dc29776648877a636
Maria <reflection2008@gmail.com>
2020-02-24 11:27:29 +0300

    Исправлены баги форм

--- build/js/script.js
+++ build/js/script.js
@@ -12,7 +12,7 @@
 }());
 
 (function () {
-  var footerButtons = document.querySelectorAll('.footer__button ');
+  var footerButtons = document.querySelectorAll('.footer__button');
   var closeAllButtons = function () {
     footerButtons.forEach(function (button) {
       if (!button.classList.contains('js-closed')) {
@@ -37,5 +37,66 @@
       infoElement.classList.remove('js-hidden');
     });
   });
-
 }());
+
+
+(function () {
+  var popupOpeners = document.querySelectorAll('.js-popup-open');
+  var popupBlock = document.querySelector('.popup');
+  var popupCloser = popupBlock.querySelector('.popup__close');
+  var overlayBlock = document.querySelector('.overlay');
+  var ESCAPE_BUTTON = 27;
+
+  var popupCloseHandler = function (evt) {
+    evt.preventDefault();
+    popupBlock.classList.remove('js-popup-show');
+    overlayBlock.removeEventListener('click', popupCloseHandler);
+    document.removeEventListener('keydown', popupEscPressHandler);
+  };
+
+  var popupEscPressHandler = function (evt) {
+    if (evt.keyCode === ESCAPE_BUTTON) {
+      popupCloseHandler(evt);
+    }
+  };
+
+  var popupHandler = function (evt) {
+    evt.preventDefault();
+    popupBlock.classList.add('js-popup-show');
+    popupCloser.addEventListener('click', popupCloseHandler);
+    overlayBlock.addEventListener('click', popupCloseHandler);
+    document.addEventListener('keydown', popupEscPressHandler);
+  };
+
+  popupOpeners.forEach(function (item) {
+    item.addEventListener('click', popupHandler);
+  });
+}());
+
+(function () {
+  window.sendForm = function (url, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.timeout = 10000;
+
+    xhr.open('GET', url);
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.send();
+  };
+
+})();
--- source/js/script.js
+++ source/js/script.js
@@ -12,7 +12,7 @@
 }());
 
 (function () {
-  var footerButtons = document.querySelectorAll('.footer__button ');
+  var footerButtons = document.querySelectorAll('.footer__button');
   var closeAllButtons = function () {
     footerButtons.forEach(function (button) {
       if (!button.classList.contains('js-closed')) {
@@ -28,14 +28,46 @@
       var infoElement = item.nextElementSibling;
       if (!item.classList.contains('js-closed')) {
         item.classList.add('js-closed');
-        infoElement.classList.remove('js-hidden');
+        infoElement.classList.add('js-hidden');
         return;
       }
 
       closeAllButtons();
       item.classList.remove('js-closed');
-      infoElement.classList.add('js-hidden');
+      infoElement.classList.remove('js-hidden');
     });
   });
+}());
 
+(function () {
+  var popupOpeners = document.querySelectorAll('.js-popup-open');
+  var popupBlock = document.querySelector('.popup');
+  var popupCloser = popupBlock.querySelector('.popup__close');
+  var overlayBlock = document.querySelector('.overlay');
+  var ESCAPE_BUTTON = 27;
+
+  var popupCloseHandler = function (evt) {
+    evt.preventDefault();
+    popupBlock.classList.remove('js-popup-show');
+    overlayBlock.removeEventListener('click', popupCloseHandler);
+    document.removeEventListener('keydown', popupEscPressHandler);
+  };
+
+  var popupEscPressHandler = function (evt) {
+    if (evt.keyCode === ESCAPE_BUTTON) {
+      popupCloseHandler(evt);
+    }
+  };
+
+  var popupHandler = function (evt) {
+    evt.preventDefault();
+    popupBlock.classList.add('js-popup-show');
+    popupCloser.addEventListener('click', popupCloseHandler);
+    overlayBlock.addEventListener('click', popupCloseHandler);
+    document.addEventListener('keydown', popupEscPressHandler);
+  };
+
+  popupOpeners.forEach(function (item) {
+    item.addEventListener('click', popupHandler);
+  });
 }());
